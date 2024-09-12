1f1549f08c2b82263bab34a15d862924e8c713f0
Lorenzzeti <129550131+Lorenzzeti@users.noreply.github.com>
2024-09-12 00:49:18 -0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,277 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLado2 = new System.Windows.Forms.Label();
+            this.lblLado1 = new System.Windows.Forms.Label();
+            this.txtLado2 = new System.Windows.Forms.TextBox();
+            this.txtLado1 = new System.Windows.Forms.TextBox();
+            this.txtPerimetro = new System.Windows.Forms.TextBox();
+            this.lblPerimetro = new System.Windows.Forms.Label();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.cmbObjetos = new System.Windows.Forms.ComboBox();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.lblRaio = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.cmbTriangulo = new System.Windows.Forms.ComboBox();
+            this.cmbForma = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblLado2);
+            this.groupBox1.Controls.Add(this.lblLado1);
+            this.groupBox1.Controls.Add(this.txtLado2);
+            this.groupBox1.Controls.Add(this.txtLado1);
+            this.groupBox1.Controls.Add(this.txtPerimetro);
+            this.groupBox1.Controls.Add(this.lblPerimetro);
+            this.groupBox1.Controls.Add(this.txtArea);
+            this.groupBox1.Controls.Add(this.lblArea);
+            this.groupBox1.Controls.Add(this.cmbObjetos);
+            this.groupBox1.Controls.Add(this.btnCriar);
+            this.groupBox1.Controls.Add(this.txtRaio);
+            this.groupBox1.Controls.Add(this.txtAltura);
+            this.groupBox1.Controls.Add(this.txtBase);
+            this.groupBox1.Controls.Add(this.lblRaio);
+            this.groupBox1.Controls.Add(this.lblAltura);
+            this.groupBox1.Controls.Add(this.lblBase);
+            this.groupBox1.Controls.Add(this.cmbTriangulo);
+            this.groupBox1.Controls.Add(this.cmbForma);
+            this.groupBox1.Location = new System.Drawing.Point(90, 43);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(864, 399);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Formas Geográficas";
+            // 
+            // lblLado2
+            // 
+            this.lblLado2.AutoSize = true;
+            this.lblLado2.Location = new System.Drawing.Point(518, 102);
+            this.lblLado2.Name = "lblLado2";
+            this.lblLado2.Size = new System.Drawing.Size(54, 20);
+            this.lblLado2.TabIndex = 16;
+            this.lblLado2.Text = "Lado2";
+            // 
+            // lblLado1
+            // 
+            this.lblLado1.AutoSize = true;
+            this.lblLado1.Location = new System.Drawing.Point(683, 102);
+            this.lblLado1.Name = "lblLado1";
+            this.lblLado1.Size = new System.Drawing.Size(54, 20);
+            this.lblLado1.TabIndex = 1;
+            this.lblLado1.Text = "Lado1";
+            // 
+            // txtLado2
+            // 
+            this.txtLado2.Location = new System.Drawing.Point(518, 125);
+            this.txtLado2.Name = "txtLado2";
+            this.txtLado2.Size = new System.Drawing.Size(148, 26);
+            this.txtLado2.TabIndex = 15;
+            this.txtLado2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLado2_KeyPress);
+            // 
+            // txtLado1
+            // 
+            this.txtLado1.Location = new System.Drawing.Point(687, 125);
+            this.txtLado1.Name = "txtLado1";
+            this.txtLado1.Size = new System.Drawing.Size(148, 26);
+            this.txtLado1.TabIndex = 14;
+            this.txtLado1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLado1_KeyPress);
+            // 
+            // txtPerimetro
+            // 
+            this.txtPerimetro.Location = new System.Drawing.Point(527, 346);
+            this.txtPerimetro.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPerimetro.Name = "txtPerimetro";
+            this.txtPerimetro.ReadOnly = true;
+            this.txtPerimetro.Size = new System.Drawing.Size(148, 26);
+            this.txtPerimetro.TabIndex = 13;
+            // 
+            // lblPerimetro
+            // 
+            this.lblPerimetro.AutoSize = true;
+            this.lblPerimetro.Location = new System.Drawing.Point(442, 346);
+            this.lblPerimetro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPerimetro.Name = "lblPerimetro";
+            this.lblPerimetro.Size = new System.Drawing.Size(77, 20);
+            this.lblPerimetro.TabIndex = 12;
+            this.lblPerimetro.Text = "Perímetro";
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(257, 346);
+            this.txtArea.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.Size = new System.Drawing.Size(148, 26);
+            this.txtArea.TabIndex = 11;
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(206, 352);
+            this.lblArea.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(43, 20);
+            this.lblArea.TabIndex = 10;
+            this.lblArea.Text = "Area";
+            // 
+            // cmbObjetos
+            // 
+            this.cmbObjetos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbObjetos.FormattingEnabled = true;
+            this.cmbObjetos.Location = new System.Drawing.Point(612, 52);
+            this.cmbObjetos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbObjetos.Name = "cmbObjetos";
+            this.cmbObjetos.Size = new System.Drawing.Size(180, 28);
+            this.cmbObjetos.TabIndex = 9;
+            this.cmbObjetos.SelectedIndexChanged += new System.EventHandler(this.cmbObjetos_SelectedIndexChanged);
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Location = new System.Drawing.Point(460, 49);
+            this.btnCriar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(112, 35);
+            this.btnCriar.TabIndex = 8;
+            this.btnCriar.Text = "Criar";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.btnCriar_Click);
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(356, 125);
+            this.txtRaio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(145, 26);
+            this.txtRaio.TabIndex = 7;
+            this.txtRaio.TextChanged += new System.EventHandler(this.txtRaio_TextChanged);
+            this.txtRaio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRaio_KeyPress);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(190, 125);
+            this.txtAltura.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(148, 26);
+            this.txtAltura.TabIndex = 6;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAltura_KeyPress);
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(23, 126);
+            this.txtBase.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(148, 26);
+            this.txtBase.TabIndex = 5;
+            this.txtBase.TextChanged += new System.EventHandler(this.txtBase_TextChanged);
+            this.txtBase.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBase_KeyPress);
+            // 
+            // lblRaio
+            // 
+            this.lblRaio.AutoSize = true;
+            this.lblRaio.Location = new System.Drawing.Point(352, 102);
+            this.lblRaio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRaio.Name = "lblRaio";
+            this.lblRaio.Size = new System.Drawing.Size(42, 20);
+            this.lblRaio.TabIndex = 4;
+            this.lblRaio.Text = "Raio";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(186, 100);
+            this.lblAltura.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(51, 20);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Location = new System.Drawing.Point(19, 101);
+            this.lblBase.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(46, 20);
+            this.lblBase.TabIndex = 2;
+            this.lblBase.Text = "Base";
+            // 
+            // cmbTriangulo
+            // 
+            this.cmbTriangulo.FormattingEnabled = true;
+            this.cmbTriangulo.Items.AddRange(new object[] {
+            "Equilátero",
+            "Isósceles",
+            "Reto"});
+            this.cmbTriangulo.Location = new System.Drawing.Point(240, 52);
+            this.cmbTriangulo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbTriangulo.Name = "cmbTriangulo";
+            this.cmbTriangulo.Size = new System.Drawing.Size(180, 28);
+            this.cmbTriangulo.TabIndex = 1;
+            this.cmbTriangulo.Visible = false;
+            // 
+            // cmbForma
+            // 
+            this.cmbForma.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbForma.FormattingEnabled = true;
+            this.cmbForma.Items.AddRange(new object[] {
+            "Circunferência",
+            "Quadrado",
+            "Retângulo",
+            "Triângulo"});
+            this.cmbForma.Location = new System.Drawing.Point(34, 52);
+            this.cmbForma.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbForma.Name = "cmbForma";
+            this.cmbForma.Size = new System.Drawing.Size(194, 28);
+            this.cmbForma.TabIndex = 0;
+            this.cmbForma.SelectedIndexChanged += new System.EventHandler(this.cmbForma_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 462);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbForma;
+        private System.Windows.Forms.ComboBox cmbTriangulo;
+        private System.Windows.Forms.Label lblRaio;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.TextBox txtPerimetro;
+        private System.Windows.Forms.Label lblPerimetro;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.ComboBox cmbObjetos;
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtLado1;
+        private System.Windows.Forms.TextBox txtLado2;
+        private System.Windows.Forms.Label lblLado2;
+        private System.Windows.Forms.Label lblLado1;
     }
 }
-
+  
